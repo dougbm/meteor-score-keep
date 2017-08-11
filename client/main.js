5aa40424096988c60bd6371236d33fc75b7d3839
Douglas <douglas.marquardt@adp.com>
2017-08-11 09:16:20 -0300

    Add App.js and MongoDB sorting option

--- client/main.js
+++ client/main.js
@@ -4,30 +4,12 @@ import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 
 import {Players} from './../imports/api/players';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import Player from './../imports/ui/Player';
-
-const renderPlayers = (playersList) => {
-  return playersList.map((player) => {
-    return (
-      <Player key={player._id} player={player}/>
-    );
-  });
-};
+import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() => {
-    let players = Players.find().fetch();
+    let players = Players.find({},{sort: {score: -1}}).fetch();
     let title = 'Score Keep';
-    let name = 'Andrew';
-    let jsx = (
-      <div>
-        <TitleBar title={title} subtitle="Created by Douglas Marquardt"/>
-        {renderPlayers(players)}
-        <AddPlayer/>
-      </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });
 });
